ec0bce1dc6a80d1ad7a7a8d05ae59280da8a2f83
AbstractniBerezozol <aleksandr.zolotarev@abstract.rs>
2024-09-03 13:18:50 +0200

    Something

--- apps/email/src/app.controller.ts
+++ apps/email/src/app.controller.ts
@@ -2,11 +2,11 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { EmailDto } from './emailDto';
 
-@Controller('sendingTestingEmail')
+@Controller('send-email')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Post('messageRequest')
+  @Post('send-email')
   async sendAnEmail(@Body() emailDto: EmailDto) {
     return this.appService.sendLetter(emailDto);
   }
--- apps/email/src/app.module.ts
+++ apps/email/src/app.module.ts
@@ -1,27 +1,27 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MailerModule } from '@nestjs-modules/mailer';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { HttpModule, HttpService } from '@nestjs/axios';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { MailerModule } from "@nestjs-modules/mailer";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { HttpModule, HttpService } from "@nestjs/axios";
 
 @Module({
   imports: [
     HttpModule,
-    ConfigModule.forRoot({ envFilePath: 'env', isGlobal: true }),
+    ConfigModule.forRoot({ envFilePath: "env", isGlobal: true }),
     MailerModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         transport: {
-          host: configService.get<string>('EMAIL_HOST'),
-          port: configService.get<number>('PORTS_NUMBER'),
+          host: configService.get<string>("EMAIL_HOST"),
+          port: configService.get<number>("PORTS_NUMBER"),
           auth: {
-            user: configService.get<string>('EMAIL_USERNAME'),
-            pass: configService.get<string>('EMAIL_PASSWORD'),
+            user: configService.get<string>("EMAIL_USERNAME"),
+            pass: configService.get<string>("EMAIL_PASSWORD"),
           },
         },
         defaults: {
-          from: `DON'T OPEN IT FROM <${configService.get<string>('EMAIL_FROM')}`,
+          from: `DON'T OPEN IT FROM <${configService.get<string>("EMAIL_FROM")}`,
         },
       }),
       inject: [ConfigService],
